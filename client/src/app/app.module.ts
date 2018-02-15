fcb1343faa1d4c9dae92cc115fc3ac809164d0e0
Kelvin Moi <kelvin.mai002@gmail.com>
2018-02-15 13:41:42 -0600

    Set up services

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { RestApiService } from './rest-api.service';
+import { DataService } from './data.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, MessageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -17,7 +20,7 @@ import { HomeComponent } from './home/home.component';
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [RestApiService, DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
