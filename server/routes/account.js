1bad9b82500d5f84654e1e751d5705ea06c36ef3
Kelvin Moi <kelvin.mai002@gmail.com>
2018-02-15 22:26:36 -0600

    Added profile to api

--- server/routes/account.js
+++ server/routes/account.js
@@ -1,17 +1,20 @@
 const router = require('express').Router(),
 	jwt = require('jsonwebtoken');
 
-const User = require('../models/user');
+const User = require('../models/user'),
+	auth = require('../middleware/auth');
 
-router.post('/signup', (req, res, next) => {
-	user = new User();
-	user.email = req.body.email;
-	user.name = req.body.name;
-	user.password = req.body.password;
+router.post('/signup', (req, res) => {
+	const { email, name, password, isSeller } = req.body;
+	user = new User({
+		email,
+		name,
+		password,
+		isSeller
+	});
 	user.picture = user.gravatar();
-	user.isSeller = req.body.isSeller;
 
-	User.findOne({ email: req.body.email }, (err, existingUser) => {
+	User.findOne({ email }, (err, existingUser) => {
 		if (existingUser) {
 			res.json({
 				success: false,
@@ -29,8 +32,9 @@ router.post('/signup', (req, res, next) => {
 	});
 });
 
-router.post('/login', (req, res, next) => {
-	User.findOne({ email: req.body.email }, (err, user) => {
+router.post('/login', (req, res) => {
+	const { email, password } = req.body;
+	User.findOne({ email }, (err, user) => {
 		if (err) throw err;
 
 		if (!user) {
@@ -39,7 +43,7 @@ router.post('/login', (req, res, next) => {
 				message: 'User not found'
 			});
 		} else {
-			let validPassword = user.comparePassword(req.body.password);
+			let validPassword = user.comparePassword(password);
 			if (!validPassword) {
 				res.json({
 					sucess: false,
@@ -58,5 +62,66 @@ router.post('/login', (req, res, next) => {
 		}
 	});
 });
+
+router
+	.route('/profile')
+	.get(auth, (req, res) => {
+		User.findOne({ _id: req.decoded.user._id }, (err, user) => {
+			res.json({
+				success: true,
+				user,
+				message: 'Success'
+			});
+		});
+	})
+	.post(auth, (req, res, next) => {
+		User.findOne({ _id: req.decoded.user._id }, (err, user) => {
+			if (err) return next(err);
+
+			const { email, password, name, isSeller } = req.body;
+			if (email) user.email = email;
+			if (name) user.name = name;
+			if (password) user.password = password;
+
+			user.isSeller = isSeller;
+
+			user.save();
+			res.json({
+				success: true,
+				message: 'Successfully edited your profile'
+			});
+		});
+	});
+
+router
+	.route('/address')
+	.get(auth, (req, res) => {
+		User.findOne({ _id: req.decoded.user._id }, (err, user) => {
+			res.json({
+				success: true,
+				address: user.address,
+				message: 'Success'
+			});
+		});
+	})
+	.post(auth, (req, res, next) => {
+		User.findOne({ _id: req.decoded.user._id }, (err, user) => {
+			if (err) return next(err);
+
+			const { addr1, addr2, city, state, country, postalCode } = req.body;
+			const { address } = user;
+			if (addr1) address.addr1 = addr1;
+			if (addr2) address.addr2 = addr2;
+			if (city) address.city = city;
+			if (country) address.country = country;
+			if (postalCode) address.postalCode = postalCode;
+
+			user.save();
+			res.json({
+				success: true,
+				message: 'Successfully edited your address'
+			});
+		});
+	});
 
 module.exports = router;
